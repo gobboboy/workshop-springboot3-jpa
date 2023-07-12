6fb5edf402d57a5a4f18473d62768afbf87556cb
Marcos Gobbo <marcoszgobbo@gmail.com>
2023-07-12 11:58:37 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entitites/User.java
+++ src/main/java/com/educandoweb/course/entitites/User.java
@@ -2,9 +2,19 @@ package com.educandoweb.course.entitites;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -81,7 +91,6 @@ public class User implements Serializable {
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
-
 
     @Override
     public boolean equals(Object obj) {
